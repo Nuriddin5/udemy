0fe408dc02095d2c559b7a3cabdbd7ec87195cdf
Nuriddin <nuriddinqurbonboyev@mail.ru>
2022-04-15 02:06:15 +0500

    git init

---
+++ src/main/java/com/nuriddin/my_teaching_project_like_udemy/MyTeachingProjectLikeUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.nuriddin.my_teaching_project_like_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTeachingProjectLikeUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTeachingProjectLikeUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuriddin/my_teaching_project_like_udemy/MyTeachingProjectLikeUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuriddin.my_teaching_project_like_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTeachingProjectLikeUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
